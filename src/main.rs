ebb9ff35377ec94dec86a769f4cd7b439b49ae63
praveen <praveenpp2929@gmail.com>
2026-05-29 20:19:13 +0200

    added

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,14 @@ fn main() {
     let (scss, t) = measure(|| {
         let (x, t) = measure(
             || //make_dep!(1 => 2,2 => 3,3=>4,4=>3,3=>5,5=>6,7=>6,8=> 7,9=>6,6=> 13,13=>14,15=>14,14=>12,16=>12,12=>11,11=>10,10=>6),
-            TaskMap::create_map(10000),
+            // TaskMap::create_map(10000),
+            make_dep!(1=>2)
         );
         println!("Inited in {t}");
 
         //let x = make_dep!(1=>2,2=>3,2=>4,3=>5,3=>6,3=>7,4=>8,4=>9,4=>10);
         //let x = make_dep!(2 => 1,1=>0,0=>2,2=>4,4=>3,3=>2);
-        let (d_g, t) = measure(|| x.0.create_graph());
+        let (d_g, t) = measure(|| x.create_graph());
         println!("Created in {t}");
         // ?????
         let (r, t) = measure(|| kosaraju(&d_g.0));
